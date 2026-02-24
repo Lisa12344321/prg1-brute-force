091bc4fa650e728f20337c72783ef34f44dd83df
Lisa12344321 <lisa.alfven@elev.ga.ntig.se>
2026-02-24 13:38:52 +0100

    funkar typ

---
+++ brute-force.py
@@ -0,0 +1,22 @@
+alfabet = "abcdefghijklmnopqrstuvwxyzåäö"
+
+krypterat = "Äya, mux ÅTi xl öXuå!?"
+ordlista = ["hej","jag", "du", "ni", "de", "dem", "dom", "han", "hon", "den", "det", "har", "ska", "inte", "att", "och", "är", "på"]
+
+for nyckel in range(0, len(alfabet)):
+    dekrypterat = ""
+    for bokstav in krypterat:
+        if bokstav.isalpha():
+            b = alfabet[(alfabet.index(bokstav.lower()) - nyckel) % 29]
+            if bokstav.isupper():
+                b = b.upper()
+                dekrypterat += b
+            else:
+                dekrypterat += b
+    
+        else:
+            dekrypterat += bokstav
+
+    print(dekrypterat)
+    if dekrypterat.lower() in ordlista:
+        print(nyckel)
